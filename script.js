4133d11b7b1999abde3069cc3eb3d7798aa0901f
VasylFedorak <67235405+VasylFedorak@users.noreply.github.com>
2020-12-29 21:37:36 +0200

    роботи над вікном переходу до вибору створення таблиці і списку

--- script.js
+++ script.js
@@ -1,35 +1,59 @@
-const getS = selector => document.querySelector(selector)
+const getS = (selector) => document.querySelector(selector);
 
-getS('.edit').onclick = function () {
-    getS('.edit_block').classList.add('show')
-    getS('.bottom_container').classList.remove('hide')
-    getS('.edit_block_textarea').value = getS('.top_container').innerHTML;
-    getS('.style_block_wrapper').classList.remove('show')
-}
+getS(".edit").onclick = function () {
+  getS(".edit_block").classList.add("show");
+  getS(".bottom_container").classList.remove("hide");
+  getS(".edit_block_textarea").value = getS(".top_container").innerHTML;
+  getS(".style_block_wrapper").classList.remove("show");
+};
 
-getS('.save').onclick = function () {
-    getS('.edit_block').classList.remove('show')
-    getS('.top_container').innerHTML = getS('.edit_block_textarea').value
-}
+getS(".save").onclick = function () {
+  getS(".edit_block").classList.remove("show");
+  getS(".top_container").innerHTML = getS(".edit_block_textarea").value;
+};
 
-getS('.style').onclick = function () {
-    getS('.bottom_container').classList.add('hide')
-    getS('.style_block_wrapper').classList.add('show')
+getS(".style").onclick = function () {
+  getS(".bottom_container").classList.add("hide");
+  getS(".style_block_wrapper").classList.add("show");
+};
 
-}
-
-getS('.colors_button').addEventListener('click', function () {
-    getS('.color_div_hiden').classList.toggle('show')
-})
-getS('.colors_button2').addEventListener('click', function () {
-    getS('.color_div_hiden').classList.toggle('show')
-})
+getS(".colors_button").addEventListener("click", function () {
+  getS(".color_div_hiden").classList.toggle("show");
+});
+getS(".main_color_div").addEventListener("click", function (event) {
+  getS(".top_container").style.color = getComputedStyle(
+    event.target
+  ).backgroundColor;
+});
+getS(".colors_button2").addEventListener("click", function () {
+  getS(".color_div_hiden2").classList.toggle("show");
+});
+getS(".main_color_div2").addEventListener("click", function (event) {
+  getS(".top_container").style.backgroundColor = getComputedStyle(
+    event.target
+  ).backgroundColor;
+});
 
 function fontSize() {
-    getS('.top_container').style.fontSize = event.target.value;
+  getS(".top_container").style.fontSize = event.target.value;
 }
 
-let fontFamily = document.getElementById('font_family');
-fontFamily.onchange = function (e) {
-    getS('.top_container').style.fontFamily = this.value;
+function fontWeight() {
+  if (event.target.checked) {
+    getS(".top_container").classList.add("bold");
+  } else {
+    getS(".top_container").classList.remove("bold");
+  }
 }
+function fontStyle() {
+  if (event.target.checked) {
+    getS(".top_container").classList.add("cursive");
+  } else {
+    getS(".top_container").classList.remove("cursive");
+  }
+}
+
+let fontFamily = document.getElementById("font_family");
+fontFamily.onchange = function (e) {
+  getS(".top_container").style.fontFamily = event.target.value;
+};
