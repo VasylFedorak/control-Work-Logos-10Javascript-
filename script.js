7da5240280c65ca29f1e8e0252e14cb595800564
VasylFedorak <67235405+VasylFedorak@users.noreply.github.com>
2020-12-29 16:50:48 +0200

    початковий функціонал для кнопок

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-const
+const getS = selector => document.querySelector(selector)
+
+getS('.edit').onclick = function () {
+    getS('.edit_block').classList.add('show')
+    getS('.bottom_container').classList.remove('hide')
+    getS('.edit_block_textarea').value = getS('.top_container').innerHTML;
+}
+
+getS('.save').onclick = function () {
+    getS('.edit_block').classList.remove('show')
+    getS('.top_container').innerHTML = getS('.edit_block_textarea').value
+}
+
+getS('.style').onclick = function () {
+    getS('.bottom_container').classList.add('hide')
+    getS('.style_block').classList.add('show')
+
+
+
+
+}
